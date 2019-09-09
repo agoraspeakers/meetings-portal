d168c20f6219fd59f3e4f2f0b0e9951226c5bcf8
Adrian Grzywaczewski <1204969@rgu.ac.uk>
2019-09-09 23:06:50 +0200

    Changed routes to use new hash notation.

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: {
     omniauth_callbacks: 'users/omniauth_callbacks',
-    :registrations => "registrations"
+    registrations: 'registrations'
   }
 
   devise_scope :user do
